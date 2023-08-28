0b1baa2946427b5230fa19cfd4a665c7715a8127
PhobiaCide <aamason86@gmail.com>
2023-08-27 20:22:37 -0500

    Update removeHtmlTags().js

--- snippets/web/removeHtmlTags().js
+++ snippets/web/removeHtmlTags().js
@@ -1,7 +1,9 @@
-function removeHtmlTags(data) {
-  const pattern = /<[^>]*>/g; // Regular expression to match HTML tags
+const removeHtmlTags = (data) => {
+  // Regular expression to match HTML tags
+  const pattern = /<[^>]*>/g; 
   const newData = data.map(item => {
-    // Use replace() to remove HTML tags from each 'shift' property
+    // Use replace() to remove HTML tags
+    // from each 'shift' property
     const cleanedShift = item.shift.replace(pattern, '');
     return { ...item, shift: cleanedShift };
   });
